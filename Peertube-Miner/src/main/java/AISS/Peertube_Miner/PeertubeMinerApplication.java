a7381d472c9b38a5c398e230d558b10ca061ff1f
Irene Vidal Jiménez <irevidjim@alum.us.es>
2026-05-03 18:40:31 +0200

    primer commit

---
+++ Peertube-Miner/src/main/java/AISS/Peertube_Miner/PeertubeMinerApplication.java
@@ -0,0 +1,13 @@
+package AISS.Peertube_Miner;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PeertubeMinerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PeertubeMinerApplication.class, args);
+	}
+
+}
