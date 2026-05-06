006a6b5cebffff82e8c0f2147b736927df297d65
Jrd545 <joaramdia@alum.us.es>
2026-05-06 12:17:31 +0200

    .

---
+++ Peertube-Miner/src/main/java/AISS/Peertube_Miner/model/ChannelResponse.java
@@ -0,0 +1,62 @@
+
+package AISS.Peertube_Miner.model;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.annotation.JsonPropertyOrder;
+
+import javax.annotation.processing.Generated;
+import java.util.List;
+
+@JsonInclude(JsonInclude.Include.NON_NULL)
+@JsonPropertyOrder({
+    "total",
+    "data"
+})
+@Generated("jsonschema2pojo")
+public class ChannelResponse {
+
+    @JsonProperty("total")
+    private Integer total;
+    @JsonProperty("data")
+    private List<Channel> data;
+
+    @JsonProperty("total")
+    public Integer getTotal() {
+        return total;
+    }
+
+    @JsonProperty("total")
+    public void setTotal(Integer total) {
+        this.total = total;
+    }
+
+    @JsonProperty("data")
+    public List<Channel> getData() {
+        return data;
+    }
+
+    @JsonProperty("data")
+    public void setData(List<Channel> data) { this.data = data; }
+
+    @Override
+    public String toString() {
+        StringBuilder sb = new StringBuilder();
+        sb.append(ChannelResponse.class.getName()).append('@').append(Integer.toHexString(System.identityHashCode(this))).append('[');
+        sb.append("total");
+        sb.append('=');
+        sb.append(((this.total == null)?"<null>":this.total));
+        sb.append(',');
+        sb.append("data");
+        sb.append('=');
+        sb.append(((this.data == null)?"<null>":this.data));
+        sb.append(',');
+        if (sb.charAt((sb.length()- 1)) == ',') {
+            sb.setCharAt((sb.length()- 1), ']');
+        } else {
+            sb.append(']');
+        }
+        return sb.toString();
+    }
+
+}
